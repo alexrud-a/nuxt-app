5cd2ea48f1acd169847d8843427b101b7d1a5be0
AleXandra Rudivickaya <rag@one-touch.ru>
2021-03-21 22:43:36 +0300

    posts

---
+++ pages/posts/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col sm="12">
+        <v-img src="https://placeimg.com/800/400/any/grayscale" />
+        <h1>
+          {{ post.title }}
+        </h1>
+        <p>
+          {{ post.body }}
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  async asyncData({$axios, params}) {
+    const post = await $axios.$get(
+      'https://jsonplaceholder.typicode.com/posts/' + params.id
+    )
+    return {post}
+  },
+  data() {
+    return {
+      post: {}
+    }
+  }
+}
+</script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col md="4" v-for="item in posts" :key="item.id">
+        <nuxt-link :to="'posts/' + item.id" class="text-decoration-none">
+          <v-card>
+            <v-img src="https://placeimg.com/200/150/any/grayscale" />
+            <v-card-title>
+              {{ item.title }}
+            </v-card-title>
+            <v-card-text>
+              {{ item.body }}
+            </v-card-text>
+          </v-card>
+        </nuxt-link>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'posts',
+  async asyncData({$axios}) {
+    const posts = await $axios.$get(
+      'https://jsonplaceholder.typicode.com/posts'
+    )
+    return {posts}
+  },
+}
+</script>
