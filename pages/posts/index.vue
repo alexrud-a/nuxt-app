0b1aa8e6cffed9b60406d6b47ec605a7080260a0
AleXandra Rudivickaya <rag@one-touch.ru>
2021-03-21 22:54:52 +0300

    posts

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -2,8 +2,8 @@
   <v-container>
     <v-row>
       <v-col md="4" v-for="item in posts" :key="item.id">
-        <nuxt-link :to="'posts/' + item.id" class="text-decoration-none">
-          <v-card>
+        <nuxt-link :to="'posts/' + item.id" class="text-decoration-none card-link">
+          <v-card class="h-100">
             <v-img src="https://placeimg.com/200/150/any/grayscale" />
             <v-card-title>
               {{ item.title }}
@@ -29,3 +29,12 @@ export default {
   },
 }
 </script>
+
+<style lang="sass">
+.card-link
+  display: block
+  height: 100%
+
+  .v-card
+    height: 100%
+</style>
